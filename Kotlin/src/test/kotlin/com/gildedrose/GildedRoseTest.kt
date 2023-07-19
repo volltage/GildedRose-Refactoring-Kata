a3e3826c45a676e7e2293fcddca82faad75949c8
Adrian Misztal <gryf06@gmail.com>
2023-07-19 23:00:41 +0200

    Add UnitTests for functionalities

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -12,7 +12,7 @@ internal class GildedRoseTest {
         val items = listOf(item)
         val app = GildedRose(items)
 
-        val foundItem = app.items.find { it -> it.name == "foo" }!!
+        val foundItem = app.items.find { it.name == "foo" }!!
         assertEquals(100, foundItem.sellIn)
         assertEquals(200, foundItem.quality)
         app.updateQuality()
@@ -23,6 +23,146 @@ internal class GildedRoseTest {
         assertEquals(198, foundItem.quality)
     }
 
+    @Test
+    fun shouldDenoteValuesOverTimeAfterSell() {
+        val item = Item("foo", 1, 200)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        val foundItem = app.items.find { it.name == "foo" }!!
+        assertEquals(1, foundItem.sellIn)
+        assertEquals(200, foundItem.quality)
+        app.updateQuality()
+        assertEquals(0, foundItem.sellIn)
+        assertEquals(199, foundItem.quality)
+        app.updateQuality()
+        assertEquals(-1, foundItem.sellIn)
+        assertEquals(197, foundItem.quality)
+    }
+
+    @Test
+    fun itemQualityCantBeNegative() {
+        val item = Item("foo", 100, 1)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+
+        val foundItem = app.items.find { it.name == "foo" }!!
+        assertEquals(0, foundItem.quality)
+    }
+
+    @Test
+    fun agedBrieShouldIncreaseItsQualityOverTime() {
+        val item = Item("Aged Brie", 10, 1)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+
+        val foundItem = app.items.find { it.name == "Aged Brie" }!!
+        assertEquals(8, foundItem.quality)
+    }
+
+    @Test
+    fun qualityOfItemCannotBeMoreThan50() {
+        val item = Item("Aged Brie", 10, 45)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+
+        val foundItem = app.items.find { it.name == "Aged Brie" }!!
+        assertEquals(50, foundItem.quality)
+        assertEquals(50, foundItem.sellIn)
+    }
+
+    @Test
+    fun backstagePassesShouldIncreaseItsQualityOverTime() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 45)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+
+        val foundItem = app.items.find { it.name == "Backstage passes to a TAFKAL80ETC concert" }!!
+        assertEquals(50, foundItem.quality)
+        assertEquals(6, foundItem.sellIn)
+    }
+
+    @Test
+    fun backstagePassesTestOfBehaviour() {
+        val item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 1)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+
+        val foundItem = app.items.find { it.name == "Backstage passes to a TAFKAL80ETC concert" }!!
+        assertEquals(11, foundItem.quality)
+        assertEquals(5, foundItem.sellIn)
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+        assertEquals(20, foundItem.quality)
+        assertEquals(2, foundItem.sellIn)
+    }
+
+
+    @Test
+    fun sulfurasTest() {
+        val item = Item("Sulfuras, Hand of Ragnaros", 10, 80)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+        app.updateQuality()
+
+        val foundItem = app.items.find { it.name == "Sulfuras, Hand of Ragnaros" }!!
+        assertEquals(80, foundItem.quality)
+        assertEquals(10, foundItem.sellIn)
+    }
+
+
+    @Test
+    fun conjuredItemsDevoteTwiceFast() {
+        val item = Item("Conjured apple", 3, 40)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        val foundItem = app.items.find { it.name == "Conjured apple" }!!
+        assertEquals(3, foundItem.sellIn)
+        assertEquals(40, foundItem.quality)
+        app.updateQuality()
+        assertEquals(2, foundItem.sellIn)
+        assertEquals(38, foundItem.quality)
+        app.updateQuality()
+        assertEquals(1, foundItem.sellIn)
+        assertEquals(36, foundItem.quality)
+        app.updateQuality()
+        assertEquals(0, foundItem.sellIn)
+        assertEquals(34, foundItem.quality)
+        app.updateQuality()
+        assertEquals(-1, foundItem.sellIn)
+        assertEquals(30, foundItem.quality)
+    }
 }
 
 
