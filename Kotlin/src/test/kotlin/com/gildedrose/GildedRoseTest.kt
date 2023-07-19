aa15818ba13c72ebdc782160f8228ed327e83783
Adrian Misztal <gryf06@gmail.com>
2023-07-19 22:28:30 +0200

    Add UnitTest

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -5,13 +5,22 @@ import org.junit.jupiter.api.Test
 
 internal class GildedRoseTest {
 
-    @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("foo", app.items[0].name)
 
+    @Test
+    fun shouldDenoteValuesOverTime() {
+        val item = Item("foo", 100, 200)
+        val items = listOf(item)
+        val app = GildedRose(items)
+
+        val foundItem = app.items.find { it -> it.name == "foo" }!!
+        assertEquals(100, foundItem.sellIn)
+        assertEquals(200, foundItem.quality)
+        app.updateQuality()
+        assertEquals(99, foundItem.sellIn)
+        assertEquals(199, foundItem.quality)
+        app.updateQuality()
+        assertEquals(98, foundItem.sellIn)
+        assertEquals(198, foundItem.quality)
     }
 
 }
