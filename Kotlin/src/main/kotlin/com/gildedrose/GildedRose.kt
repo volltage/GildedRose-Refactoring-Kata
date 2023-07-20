f5c4b249b95a2f9059b551c9b7546bb050bbaada
Adrian Misztal <gryf06@gmail.com>
2023-07-21 00:11:18 +0200

    Refactor 7

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -15,26 +15,32 @@ class GildedRose(var items: List<Item>) {
             val itsNotSulfuras = currentItem.name != SULFURAS_NAME
             currentItem.sellIn = if (itsNotSulfuras) currentItem.sellIn - 1 else return
 
-            if (itsAgedBrie || itsTicket || currentItem.quality <= 0) {
-                if (currentItem.quality < 50) {
-                    currentItem.quality = currentItem.quality + 1
-                    if (itsTicket && currentItem.quality < 50 && currentItem.sellIn < 5) {
-                        currentItem.quality = currentItem.quality + 1
+            if (itsTicket) {
+                if (currentItem.sellIn < 0) {
+                    currentItem.quality = 0
+                } else if (currentItem.quality < 50) {
+                    currentItem.quality++
+                    if (currentItem.quality < 50 && currentItem.sellIn < 5) {
+                        currentItem.quality++
                     }
-                    if (itsTicket && currentItem.quality < 50 && currentItem.sellIn < 10) {
-                        currentItem.quality = currentItem.quality + 1
+                    if (currentItem.quality < 50 && currentItem.sellIn < 10) {
+                        currentItem.quality++
                     }
                 }
+            } else if (itsAgedBrie && currentItem.quality < 50) {
+                currentItem.quality++
+                if (currentItem.sellIn < 0) {
+                    currentItem.quality++
+                }
+            } else if (currentItem.quality <= 0) {
+                currentItem.quality++
             } else {
-                currentItem.quality = currentItem.quality - 1
+                currentItem.quality--
             }
 
-            if (currentItem.sellIn < 0 && itsAgedBrie && currentItem.quality < 50) currentItem.quality =
-                currentItem.quality + 1
-            else if (currentItem.sellIn < 0 && itsTicket) currentItem.quality = 0
-            else if (currentItem.sellIn < 0 && currentItem.quality > 0) currentItem.quality =
-                currentItem.quality - 1
-
+            if (currentItem.sellIn < 0 && currentItem.quality > 0) {
+                currentItem.quality--
+            }
 
         }
     }
